2d8cb2be6abb5cbaf88ebfafea1f5296e156650b
mrinal <iammrinalkc@gmail.com>
2021-12-18 22:52:04 +0530

    strings

---
+++ 1_Js Introduction/4_strings.js
@@ -0,0 +1,101 @@
+//String is a sequence of characters
+
+// let str = 'pepcoders'
+
+// console.log(str)
+
+//Methods -
+
+//Length of string
+//console.log('The length of string is' ,str.length)
+
+//extracting a part of a string
+
+//1 .//slice
+// slice(start , end+1)
+
+// let slicedStr = str.slice(3)
+// console.log(slicedStr)
+
+// //2. substr
+// // substr(start , length)
+// let subString = str.substr(3)
+// console.log(subString)
+
+
+//Replacing String Content
+
+// let sayHello = 'Hello Palak'
+
+// console.log(sayHello) // Hello Palak
+
+//Replace Method
+
+//repalce(valueTobeReplaced , valueTobeReplacedWith)
+
+// let sayBye = sayHello.replace('Hello' , 'Bye')
+
+// console.log(sayBye) // Bye Palak
+
+
+
+//ToUpperCase and ToLowerCase
+
+// let text1 = 'Hello World'
+// let text2 = text1.toUpperCase()
+
+// console.log(text2)
+
+// toLowerCase
+
+
+
+// concatenation (Concat Method)
+
+// let firstString = 'Hello'
+// let secondString = 'Ayush'
+
+// // Hello Ayush
+
+// let cocatenation = firstString.concat(" bhai " , secondString)
+// console.log(cocatenation)
+
+
+//Trim method is used to remove all the whiteSpaces
+
+let text = '            Hello World          '
+console.log(text)
+
+let trimmedtext = text.trim()
+console.log(trimmedtext)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
