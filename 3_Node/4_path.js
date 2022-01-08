a98a187191fbad1989d047a21665198566490bf0
mrinal1224 <iammrinalkc@gmail.com>
2022-01-08 22:20:08 +0530

    path

---
+++ 3_Node/4_path.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+
+let ext = path.extname('D:\\Batches\\FJP3 Dev\\3_Node\\f1.txt')
+// this extname method gets the extension name of the file
+
+console.log('Extension Name->'+ ext)
+
+
+let basename = path.basename('D:\\Batches\\FJP3 Dev\\3_Node\\f1.txt')
+console.log('BaseName->'+ basename)
+
+
+console.log(__dirname) // gets you the path of the current directory of the file
+console.log(__filename) // gets you the path of the file you are in
