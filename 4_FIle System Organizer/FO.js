cc4405aeb7e2dd50b9836801dc6150005939b25d
mrinal1224 <iammrinalkc@gmail.com>
2022-01-18 21:50:17 +0530

    file system 1

--- 4_FIle System Organizer/FO.js
+++ 4_FIle System Organizer/FO.js
@@ -7,21 +7,25 @@
 
 //js mein input Array ke from mein jaata hai and that is array is process.argv Array
 
+const fs = require("fs");
+
+const path = require("path");
+
 let inputArr = process.argv.slice(2);
 
 //[Node FO.js tree folderpath]
 
-let command = inputArr[0]
+let command = inputArr[0];
 
 switch (command) {
   case "tree":
     console.log("Tree Implemented");
     break;
   case "organize":
-    console.log("Organize Implememted");
+    organizeFn(inputArr[1]);
     break;
   case "help":
-    helpfn()
+    helpfn();
     break;
 
   default:
@@ -29,17 +33,33 @@ switch (command) {
     break;
 }
 
-
-
-function helpfn(){
-   console.log(`List of all the Commands-
+function helpfn() {
+  console.log(`List of all the Commands-
                     1) Tree Command - node FO.js tree <dirname>
                     2) Organize Command- node FO.js organize <dirname>
-                    3) Help Command - node FO.js help`)
+                    3) Help Command - node FO.js help`);
 }
 
+function organizeFn(dirpath) {
+  let destPath;
 
+  if (dirpath == undefined) {
+    console.log("Please Enter a valid Directory Path");
+    return;
+  } else {
+    let doesExist = fs.existsSync(dirpath);
+    //console.log(doesExist);
 
+    if (doesExist == true) {
+      destPath = path.join(dirpath, "organized_files");
 
-
-
+      if (fs.existsSync(destPath) == false) {
+        fs.mkdirSync(destPath);
+      } else {
+        console.log("This folder Alredy Exists");
+      }
+    } else {
+      console.log("Please Enter a valid Path");
+    }
+  }
+}
