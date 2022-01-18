ae37d1962652a67817c730d9d58b6d7021ae06d4
mrinal1224 <iammrinalkc@gmail.com>
2022-01-18 23:42:23 +0530

    organize complete

--- 4_FIle System Organizer/FO.js
+++ 4_FIle System Organizer/FO.js
@@ -110,7 +110,11 @@ function organizeHelper(src , dest){
           if(isFile==true){
                  let fileCategory = getCategory(childNames[i]);
                  console.log(childNames[i]+ "  belongs to  " + fileCategory)
+
+                 sendFiles(childAddress , dest , fileCategory)
           }
+
+
    }
 }
 
@@ -139,4 +143,26 @@ function getCategory(name){
    return 'others'
 
 
+}
+
+
+function sendFiles(srcFilePath , dest , fileCategory){
+       let catPath = path.join(dest, fileCategory)
+
+
+       if(fs.existsSync(catPath)==false){ // checking for category folder path 
+              fs.mkdirSync(catPath)
+       }
+
+
+       let fileName = path.basename(srcFilePath) /// we took out the names of the files
+       let destFilePath = path.join(catPath , fileName) // here we created a path for the files in category folders
+
+
+       fs.copyFileSync(srcFilePath , destFilePath) // copied files from src to dest
+
+       fs.unlinkSync(srcFilePath) // deleted the files from src
+
+
+       console.log(fileName + "is copied to" + fileCategory)
 }
