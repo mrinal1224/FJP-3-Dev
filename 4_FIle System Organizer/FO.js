3002e0c00736841ace0e3f6efd52883fdbc3015b
mrinal1224 <iammrinalkc@gmail.com>
2022-01-20 22:27:07 +0530

    help module

--- 4_FIle System Organizer/FO.js
+++ 4_FIle System Organizer/FO.js
@@ -7,12 +7,17 @@
 
 //js mein input Array ke from mein jaata hai and that is array is process.argv Array
 
+const helpModule = require('./commands/help')
+
+const organizeModule = require('./commands/organize')
+
+const treeModule = require('./commands/tree')
+
 const fs = require("fs");
 
 const path = require("path");
 
 let inputArr = process.argv.slice(2);
-
 
 let types = {
   media: ["mp4", "mkv", "mp3"],
@@ -41,13 +46,13 @@ let command = inputArr[0];
 
 switch (command) {
   case "tree":
-    treeFn(inputArr[1])
+    treeFn(inputArr[1]);
     break;
   case "organize":
     organizeFn(inputArr[1]);
     break;
   case "help":
-    helpfn();
+    helpModule.helpKey()
     break;
 
   default:
@@ -55,15 +60,10 @@ switch (command) {
     break;
 }
 
-function helpfn() {
-  console.log(`List of all the Commands-
-                    1) Tree Command - node FO.js tree <dirname>
-                    2) Organize Command- node FO.js organize <dirname>
-                    3) Help Command - node FO.js help`);
-}
+
 
 function organizeFn(dirpath) {
-       // input of a directory Path
+  // input of a directory Path
   let destPath;
 
   if (dirpath == undefined) {
@@ -74,7 +74,6 @@ function organizeFn(dirpath) {
     let doesExist = fs.existsSync(dirpath);
 
     // this will tell whether the dirpath exsists or not
-    
 
     if (doesExist == true) {
       destPath = path.join(dirpath, "organized_files");
@@ -91,130 +90,101 @@ function organizeFn(dirpath) {
     }
   }
 
-  organizeHelper(dirpath , destPath)
-
-
+  organizeHelper(dirpath, destPath);
 }
 
 // we are writing this function to categorize our files
-function organizeHelper(src , dest){
-   let childNames = fs.readdirSync(src) // get all the files and folders inside your src
-   //console.log(childNames) 
-   
-   for(let i=0 ; i<childNames.length ; i++){
-          let childAddress = path.join(src , childNames[i]) // path is identified for the files
-          let isFile = fs.lstatSync(childAddress).isFile() // we check here to identify only the files
-          //console.log(childAddress + "  " + isFile)
+function organizeHelper(src, dest) {
+  let childNames = fs.readdirSync(src); // get all the files and folders inside your src
+  //console.log(childNames)
 
+  for (let i = 0; i < childNames.length; i++) {
+    let childAddress = path.join(src, childNames[i]); // path is identified for the files
+    let isFile = fs.lstatSync(childAddress).isFile(); // we check here to identify only the files
+    //console.log(childAddress + "  " + isFile)
 
-          if(isFile==true){
-                 let fileCategory = getCategory(childNames[i]); //.txt
-                 console.log(childNames[i]+ "  belongs to  " + fileCategory)
-                 // we took out all the category type of different files
+    if (isFile == true) {
+      let fileCategory = getCategory(childNames[i]); //.txt
+      console.log(childNames[i] + "  belongs to  " + fileCategory);
+      // we took out all the category type of different files
 
-                 sendFiles(childAddress , dest , fileCategory)
-          }
-
-
-   }
+      sendFiles(childAddress, dest, fileCategory);
+    }
+  }
 }
 
+function getCategory(name) {
+  let ext = path.extname(name);
+  ext = ext.slice(1); // we will take out the extension names of the files
+  //console.log(ext)
 
-function getCategory(name){
-       let ext = path.extname(name)
-       ext = ext.slice(1)  // we will take out the extension names of the files 
-       //console.log(ext)
-  
+  for (let type in types) {
+    let cTypeArr = types[type];
+    //console.log(cTypeArr)
 
-       for(let type in types){
-              let cTypeArr = types[type]
-              //console.log(cTypeArr)
+    for (let i = 0; i < cTypeArr.length; i++) {
+      if (ext == cTypeArr[i])
+        // we matched the extensions with the values presnet in ctypeArr
 
-              for(let i=0 ; i<cTypeArr.length ;i++){
-                     if(ext == cTypeArr[i])
-                     // we matched the extensions with the values presnet in ctypeArr
+        return type;
+    }
+  }
 
-                     return type
-              }
-       }
-
-
-
-
-   return 'others'
-
-
+  return "others";
 }
 
+function sendFiles(srcFilePath, dest, fileCategory) {
+  let catPath = path.join(dest, fileCategory); // here we are making file categories paths
 
-function sendFiles(srcFilePath , dest , fileCategory){
-       let catPath = path.join(dest, fileCategory) // here we are making file categories paths
+  if (fs.existsSync(catPath) == false) {
+    // checking for category folder path
+    fs.mkdirSync(catPath);
+  }
 
+  let fileName = path.basename(srcFilePath); /// we took out the names of the files
+  let destFilePath = path.join(catPath, fileName); // here we created a path for the files in category folders
 
-       if(fs.existsSync(catPath)==false){ // checking for category folder path 
-              fs.mkdirSync(catPath)
-       }
+  fs.copyFileSync(srcFilePath, destFilePath); // copied files from src to dest
 
+  fs.unlinkSync(srcFilePath); // deleted the files from src
 
-       let fileName = path.basename(srcFilePath) /// we took out the names of the files
-       let destFilePath = path.join(catPath , fileName) // here we created a path for the files in category folders
-
-
-       fs.copyFileSync(srcFilePath , destFilePath) // copied files from src to dest
-
-       fs.unlinkSync(srcFilePath) // deleted the files from src
-
-
-       console.log(fileName + "is copied to" + fileCategory)
+  console.log(fileName + "is copied to" + fileCategory);
 }
 
-
-
-function treeFn(dirpath){
-   if(dirpath==undefined){
-     console.log('Please Enter a Valid Command ')
-   }
-
-   else {
-     let doesExist = fs.existsSync(dirpath);
-     if(doesExist==true){
-       treeHelper(dirpath , " ")
-     }
-   }
+function treeFn(dirpath) {
+  if (dirpath == undefined) {
+    console.log("Please Enter a Valid Command ");
+  } else {
+    let doesExist = fs.existsSync(dirpath);
+    if (doesExist == true) {
+      treeHelper(dirpath, " ");
+    }
+  }
 }
 
+function treeHelper(targetPath, indent) {
+  let isFile = fs.lstatSync(targetPath).isFile();
+  // here we have checked wheter the targetPath is a file or a folder
 
-function treeHelper(targetPath , indent){
-     let isFile = fs.lstatSync(targetPath).isFile()
-     // here we have checked wheter the targetPath is a file or a folder
+  if (isFile == true) {
+    let fileName = path.basename(targetPath);
+    console.log(indent + "├──" + fileName);
+    // this will display the files
+  } else {
+    let dirName = path.basename(targetPath);
+    console.log(indent + "└──" + dirName);
+    // this will display the folders
 
-     if(isFile==true){
-          let fileName = path.basename(targetPath)
-          console.log(indent + "├──"+ fileName)
-          // this will display the files
-     }
-     else{
-         let dirName = path.basename(targetPath);
-         console.log(indent + '└──' + dirName)
-         // this will display the folders
+    let children = fs.readdirSync(targetPath);
+    //console.log(children)
+    // here we took out all the children of test folder
 
+    for (let i = 0; i < children.length; i++) {
+      let childPath = path.join(targetPath, children[i]);
 
-         let children = fs.readdirSync(targetPath)
-         //console.log(children)
-         // here we took out all the children of test folder
-
-
-         for(let i=0 ; i<children.length ; i++){
-           let childPath = path.join(targetPath , children[i]);
-
-           //console.log(childPath)
-           treeHelper(childPath , indent + '\t')
-           // using Recursion to repeat the process for all files and Folders
-         }
-     }
+      //console.log(childPath)
+      treeHelper(childPath, indent + "\t");
+      // using Recursion to repeat the process for all files and Folders
+    }
+  }
 }
-
-
-
-
-
---
+++ 4_FIle System Organizer/commands/help.js
@@ -0,0 +1,10 @@
+function helpfn() {
+       console.log(`List of all the Commands-
+                         1) Tree Command - node FO.js tree <dirname>
+                         2) Organize Command- node FO.js organize <dirname>
+                         3) Help Command - node FO.js help`);
+     }
+
+module.exports={
+       helpKey : helpfn
+}
