b6aeb999c35482a27e0efd4d24cddf05fb02d8bd
mrinal1224 <iammrinalkc@gmail.com>
2022-02-03 22:56:54 +0530

    espn2

--- 7_webScraping/espnScraper/main.js
+++ 7_webScraping/espnScraper/main.js
@@ -19,8 +19,39 @@ function extractLink(html) {
 
   let link = anchorElem.attr("href");
 
-  //console.log(link)
+  console.log(link)
 
-  let fullLink = "https://www.espncricinfo.com/" + link;
+  let fullLink = "https://www.espncricinfo.com" + link;
   console.log(fullLink);
+
+
+  getAllMatchLink(fullLink)
+}
+
+function getAllMatchLink(uri){
+        request(uri , function(error , response , html){
+               if(error){
+                      console.log(error)
+               }
+               else{
+                      extractAllLink(html)
+               }
+        } )
+}
+
+
+function extractAllLink(html){
+   let $ = cheerio.load(html)
+
+   let scoreCardArr = $('a[data-hover="Scorecard"]')
+
+   for(let i=0 ; i<scoreCardArr.length ; i++){
+          let link = $(scoreCardArr[i]).attr('href')
+          let fullLink = 'https://www.espncricinfo.com/'+link
+          console.log(fullLink)
+   }
+
+
+
+   
 }
