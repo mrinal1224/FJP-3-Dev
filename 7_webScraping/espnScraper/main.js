e468864c9f2653780af53d07324b8fa2eedba816
mrinal1224 <iammrinalkc@gmail.com>
2022-02-03 22:14:31 +0530

    espn

---
+++ 7_webScraping/espnScraper/main.js
@@ -0,0 +1,26 @@
+const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595";
+
+const request = require("request");
+const cheerio = require("cheerio");
+
+request(url, cb);
+
+function cb(err, response, html) {
+  if (err) {
+    console.error(err);
+  } else {
+    extractLink(html);
+  }
+}
+
+function extractLink(html) {
+  let $ = cheerio.load(html);
+  let anchorElem = $('a[data-hover="View All Results"]');
+
+  let link = anchorElem.attr("href");
+
+  //console.log(link)
+
+  let fullLink = "https://www.espncricinfo.com/" + link;
+  console.log(fullLink);
+}
