a66b3e25de3d91e13a5df7057e10990cfe10d68c
mrinal1224 <iammrinalkc@gmail.com>
2022-02-08 21:51:02 +0530

    espn

--- 7_webScraping/espnScraper/scorecard.js
+++ 7_webScraping/espnScraper/scorecard.js
@@ -1,5 +1,5 @@
-const url = 'https://www.espncricinfo.com//series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard'
-
+const url =
+  "https://www.espncricinfo.com//series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard";
 
 const request = require("request");
 const cheerio = require("cheerio");
@@ -10,26 +10,46 @@ function cb(err, response, html) {
   if (err) {
     console.error(err);
   } else {
-     extractMatchDetails(html)
+    extractMatchDetails(html);
   }
 }
 
+function extractMatchDetails(html) {
+  let $ = cheerio.load(html);
 
-function extractMatchDetails(html){
-       let $ = cheerio.load(html)
+  let descString = $('.match-header-info.match-info-MATCH .description');
 
-       let descString = $('.header-info .description')
-    
-       let descStringArr = descString.text().split(',')
+  let descStringArr = descString.text().split(",");
 
-       let venue = descStringArr[1].trim()
-       let date = descStringArr[2].trim()
+  let venue = descStringArr[1].trim();
+  let date = descStringArr[2].trim();
 
-       let result = $('.match-info.match-info-MATCH.match-info-MATCH-half-width .status-text span').text()
+  let result = $(
+    ".match-info.match-info-MATCH.match-info-MATCH-half-width .status-text span"
+  ).text();
+
+  console.log(venue);
+  console.log(date);
+  console.log(result);
+
+  console.log('```````````````````````````````````````````````````')
+
+
+  let innings = $('.card.content-block.match-scorecard-table>.Collapsible')
+
+
+  let htmlString = ''
+
+
+  for(let i=0 ; i<innings.length ; i++){
+      htmlString += $(innings[i]).html()
+  }
+
+  console.log(htmlString)
 
 
 
-       console.log(venue)
-       console.log(date)
-       console.log(result)
+
+
+
 }
