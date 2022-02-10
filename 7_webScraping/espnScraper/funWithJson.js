4a333fa15429f1ca462eec9f3872a77a3d389435
mrinal1224 <iammrinalkc@gmail.com>
2022-02-10 22:16:43 +0530

    json

--- 7_webScraping/espnScraper/funWithJson.js
+++ 7_webScraping/espnScraper/funWithJson.js
@@ -1,40 +1,39 @@
-const fs = require('fs')
+const fs = require("fs");
+
+// let buffer = fs.readFileSync("./example.json");
+
+// //console.log(buffer)
+
+// let data = JSON.parse(buffer);
+ // This methods is used to covert buffer or any type of  data to json
 
 
-let buffer = fs.readFileSync('./example.json')
 
-//console.log(buffer)
+let jsonFile = require('./example.json') // requiring JSON File
+//console.log(data)
 
-let data = JSON.parse(buffer)
+jsonFile.push({
+  name: "Thor",
+  "last name": "Odinson",
+  isAvenger: true,
+  friends: ["Tony", "Peter", "Bruce"],
+  age: 102,
+  address: {
+    planet: "Asgard",
+  },
+});
+
+// pushing a new object into JSON File
 
 //console.log(data)
 
-data.push({
-       "name" : 'Thor',
-       "last name" : 'Odinson',
-       "isAvenger": true,
-              "friends" : ["Tony" , "Peter" , "Bruce"],
-              "age" : 102,
-              "address":{
-                     "planet" : 'Asgard'
-       
-              }
-
-
-})
-
-
-//console.log(data)
-
-
-let stringData = JSON.stringify(data)
+let stringData = JSON.stringify(jsonFile);
+// converting json data into string so that we can write it in other files
 
 //console.log(stringData)
 
-fs.writeFileSync("example.json" , stringData)
+fs.writeFileSync("example.json", stringData);
 
-console.log('JSON file Updated')
+// writing to json file
 
-
-
-
+console.log("JSON file Updated");
