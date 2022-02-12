d1089d407dd3b0e4836d4047dff5b27287ccbafa
mrinal1224 <iammrinalkc@gmail.com>
2022-02-12 21:51:43 +0530

    mapFilterQuestions

---
+++ 5_Js Concepts/Questions/2_mapFilter.js
@@ -0,0 +1,24 @@
+
+//Map Question
+
+
+//You have to use map function and have to get all the students name in upperCase
+
+
+
+let studentRecords = [
+  { name: "Abhishek", id: 123, marks: 98 },
+  { name: "Udai", id: 101, marks: 90 },
+  { name: "Himanshu", id: 200, marks: 96 },
+  { name: "Mrinal", id: 115, marks: 75 },
+];
+
+
+
+
+
+
+// Filter
+
+//Retrieve the details of students who scored more than 50 marks and have id greater than 120 from studentRecord
+//use filter method to approach the problem
--- 5_Js Concepts/Questions/mapFIlterQues.js
+++ 5_Js Concepts/Questions/mapFIlterQues.js
@@ -1,27 +1,33 @@
 //Get the movie Names from this Array of Objects
-//method and only get the movie name which has rating higher than or equal to 8
+// only get the movie name which has rating higher than or equal to 8
 // use Filter and map  chaining to achaive the objective
 
-
 var newReleases = [
-       {
-              "id": 1,
-              "title": "Die Hard",
-               "rating":9
-              
-       },
-       {
-              "id": 2,
-              "title": "Bad Boys",
-               "rating":7
-              
-       },
-       {
-              "id": 3,
-              "title": "The Chamber",
-              "rating": 10
-              
-       },
-       
+  {
+    id: 1,
+    title: "Die Hard",
+    rating: 9,
+  },
+  {
+    id: 2,
+    title: "Bad Boys",
+    rating: 7,
+  },
+  {
+    id: 3,
+    title: "The Chamber",
+    rating: 10,
+  },
 ];
 
+// Solution -
+
+let moviesGreaterThan8 = newReleases
+  .filter(function (movie) {
+    return movie.rating >= 8;
+  })
+  .map(function (movie) {
+    return movie.title;
+  });
+
+console.log(moviesGreaterThan8);
