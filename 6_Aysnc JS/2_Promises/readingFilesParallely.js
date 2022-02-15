e16d437e7549b64a4538cc75e64884d98babbdba
mrinal1224 <iammrinalkc@gmail.com>
2022-02-15 23:16:51 +0530

    microTask

---
+++ 6_Aysnc JS/2_Promises/readingFilesParallely.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+let f1p = fs.promises.readFile("f1.txt");
+
+let f2p = fs.promises.readFile("f2.txt");
+
+let f3p = fs.promises.readFile("f3.txt");
+
+function cb(data) {
+  console.log("File Data -> " + data);
+}
+
+f1p.then(cb);
+
+f2p.then(cb);
+
+f3p.then(cb);
