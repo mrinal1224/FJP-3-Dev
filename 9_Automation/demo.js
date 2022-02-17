8bb58f87c956e08b62e58e096dae27cf59e1a486
mrinal1224 <iammrinalkc@gmail.com>
2022-02-17 22:44:52 +0530

    automation

--- 9_Automation/demo.js
+++ 9_Automation/demo.js
@@ -3,11 +3,21 @@ let puppeteer = require('puppeteer')
 console.log('Before')
 
 let browserWillbeLauncedPromise = puppeteer.launch({
-       headless:false
+       headless:false,
+       defaultViewport:null,
+       rgs: ['--start-maximized']
 })
 
 browserWillbeLauncedPromise.then(function(browserInstance){
-       return browserInstance;
+       let newTabPromise= browserInstance.newPage()
+       return newTabPromise;
+}).then(function (newTab){
+  console.log('New Tab opened')
+  let pageWillbeOpenedPromise = newTab.goto('https://www.pepcoding.com/')
+  return pageWillbeOpenedPromise
+
+}).then(function(webPage){
+       console.log('Website opened')
 })
 
 
