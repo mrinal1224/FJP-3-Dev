9d58d0e8df07c521fd5361b86eba751b790c2d6b
mrinal1224 <iammrinalkc@gmail.com>
2022-02-17 21:26:45 +0530

    promise serial

--- 6_Aysnc JS/2_Promises/readingFilesSerially.js
+++ 6_Aysnc JS/2_Promises/readingFilesSerially.js
@@ -1,33 +1,27 @@
-const fs = require("fs");
+let fs = require("fs");
 
-console.log("Before");
+console.log("before");
 
-fs.readFile("f1.txt", cb1);
+let f1p = fs.promises.readFile("f1.txt");
 
-function cb1(err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(" " + data);
-    fs.readFile("f2.txt", cb2);
-  }
+f1p.then(cb);
+
+function cb(data) {
+  console.log("File data -> " + data);
+  let f2p = fs.promises.readFile("f2.txt");
+
+  f2p.then(cb2);
 }
 
-function cb2(err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(" " + data);
-    fs.readFile("f3.txt", cb3);
-  }
+function cb2(data) {
+  console.log("File data -> " + data);
+  let f3p = fs.promises.readFile("f3.txt");
+
+  f3p.then(cb3);
 }
 
-function cb3(err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(" " + data);
-  }
+function cb3(data) {
+  console.log("File data -> " + data);
 }
 
-console.log("After");
+console.log("after");
---
+++ 6_Aysnc JS/callbacks/serialReading.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+console.log("Before");
+
+fs.readFile("f1.txt", cb1);
+
+function cb1(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(" " + data);
+    fs.readFile("f2.txt", cb2);
+  }
+}
+
+function cb2(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(" " + data);
+    fs.readFile("f3.txt", cb3);
+  }
+}
+
+function cb3(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(" " + data);
+  }
+}
+
+console.log("After");
