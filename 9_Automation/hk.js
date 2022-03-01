30ef48e901d2e8032526ff1cce490def6b9203a9
mrinal1224 <iammrinalkc@gmail.com>
2022-03-01 22:35:56 +0530

    hackerRank Promises

--- 9_Automation/hk.js
+++ 9_Automation/hk.js
@@ -73,6 +73,7 @@ browserWillbeLauncedPromise
       questionsArr[0],
       codeFile.answers[0]
     );
+    return questionWillBeSolvedPromise
   });
 
 function waitAndClick(selector, cPage) {
@@ -113,8 +114,8 @@ function questionSolver(page, question, answer) {
         return page.type(".text-area.custominput", answer, { delay: 20 });
       })
       .then(function () {
-        let ctrlIsPressedPromise = page.keyboard.down('Control');
-        return ctrlIsPressedPromise
+        let ctrlonHoldPromise = page.keyboard.down('Control');
+        return ctrlonHoldPromise
       }).then(function(){
         let AisPressedPromise = page.keyboard.press('A' , {delay : 20});
         return AisPressedPromise
@@ -122,7 +123,32 @@ function questionSolver(page, question, answer) {
          let XisPressedPromise = page.keyboard.press('X' , {delay:20})
          return XisPressedPromise
       }).then(function(){
-        console.log('Cut Implemented')
-      });
+         let ctrlIsReleasedPromise = page.keyboard.up('Control')
+         return ctrlIsReleasedPromise
+      }).then(function () {
+        let waitForEditorPromise = waitAndClick(
+          ".monaco-editor.no-user-select.vs",
+          page
+        );
+        return waitForEditorPromise;
+      }).then(function () {
+        let ctrlonHoldPromise = page.keyboard.down('Control');
+        return ctrlonHoldPromise
+      }).then(function(){
+        let AisPressedPromise = page.keyboard.press('A' , {delay : 20});
+        return AisPressedPromise
+      }).then(function(){
+        let VisPressedPromise = page.keyboard.press('V' , {delay:20})
+        return VisPressedPromise
+     }).then(function(){
+      let ctrlIsReleasedPromise = page.keyboard.up('Control')
+      return ctrlIsReleasedPromise
+   }).then(function(){
+      return page.click('.hr-monaco__run-code' , {delay : 20})
+   }).then(function(){
+     resolve()
+   }).catch(function(err){
+     console.log(err)
+   })
   });
 }
