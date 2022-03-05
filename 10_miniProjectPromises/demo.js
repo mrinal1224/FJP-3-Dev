e97f11136978d514923901c1d45bf6e086c766d4
mrinal1224 <iammrinalkc@gmail.com>
2022-03-05 22:01:38 +0530

    demo inquirer

---
+++ 10_miniProjectPromises/demo.js
@@ -0,0 +1,19 @@
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+           type : 'list',
+           name : 'selection',
+           choices : ['choice 1' , 'choice 2']
+
+    }
+  ])
+  .then(function(ans){
+     if(ans.selection == 'choice 1'){
+            console.log('Choice 1 selected')
+     }
+     else {
+            console.log('Choice 2 selected')
+     }
+  })
+  
