8b65d7fa33656c82f5cabfb758e40dea6dc87d17
mrinal1224 <iammrinalkc@gmail.com>
2022-03-23 23:16:14 +0530

    modal show and hide

--- 11_Browser/Project 1 - Jira Ticket Clone/script.js
+++ 11_Browser/Project 1 - Jira Ticket Clone/script.js
@@ -1,7 +1,26 @@
 let addBtn = document.querySelector('.add-btn')
 
+let modalCont = document.querySelector('.modal-cont')
+
+let addFlag = false
+
 addBtn.addEventListener('click' , function(e){
-       console.log('Linked')
+       //Display the Modal
+
+  // addFlag , true - Modal Display
+  //addFlag , false - Modal Hide
+
+  addFlag = !addFlag
+
+ if(addFlag==true){
+   modalCont.style.display = 'flex'
+ }
+
+ else{
+       modalCont.style.display = 'none'
+ }
+
+
 
 
 
