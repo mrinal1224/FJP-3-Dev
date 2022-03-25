f9cc650c2650d7fc57e5102d11bf554c635f5e41
mrinal1224 <iammrinalkc@gmail.com>
2022-03-25 22:38:27 +0530

    active color

--- 11_Browser/Project 1 - Jira Ticket Clone/script.js
+++ 11_Browser/Project 1 - Jira Ticket Clone/script.js
@@ -2,7 +2,12 @@ let addBtn = document.querySelector(".add-btn");
 
 let modalCont = document.querySelector(".modal-cont");
 
-let mainCont = document.querySelector('.main-cont')
+let mainCont = document.querySelector(".main-cont");
+
+let colors = ['lightpink' , 'lightblue' , 'lightgreen' , 'black']
+let modalPriorityColor = colors[colors.length-1] // black
+
+let allPriorityColors = document.querySelectorAll('.priority-color')
 
 let addFlag = false;
 
@@ -22,30 +27,40 @@ addBtn.addEventListener("click", function (e) {
 });
 
 
+//Changing Priority Colors
 
-// Generating a Ticket
-
-modalCont.addEventListener('keydown' , function(e){
-  let key = e.key
-
-  if(key=='Shift'){
-    createTicket() // this function will generate the ticket
-    modalCont.style.display='none'
-  }
+allPriorityColors.forEach(function(colorElem){
+  colorElem.addEventListener('click' , function(e){
+       allPriorityColors.forEach(function(priorityColorElem){
+          priorityColorElem.classList.remove('active')
+       })
+       colorElem.classList.add('active')
+  })
 })
 
 
 
-function createTicket(){
-  let ticketCont = document.createElement('div')
-  ticketCont.setAttribute('class' , 'ticket-cont')
+
+
+// Generating a Ticket
+
+modalCont.addEventListener("keydown", function (e) {
+  let key = e.key;
+
+  if (key == "Shift") {
+    createTicket(); // this function will generate the ticket
+    modalCont.style.display = "none";
+    addFlag = false
+  }
+});
+
+function createTicket() {
+  let ticketCont = document.createElement("div");
+  ticketCont.setAttribute("class", "ticket-cont");
 
   ticketCont.innerHTML = `<div class="ticket-color"></div>
   <div class="ticket-id"></div>
-  <div class="task-area"></div>`
+  <div class="task-area"></div>`;
 
-  mainCont.appendChild(ticketCont)
-
-
+  mainCont.appendChild(ticketCont);
 }
-
