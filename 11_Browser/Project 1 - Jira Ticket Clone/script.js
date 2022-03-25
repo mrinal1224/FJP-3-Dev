563d060da7ab0bcac9e9fa33b096b091f128ee62
mrinal1224 <iammrinalkc@gmail.com>
2022-03-25 21:51:10 +0530

    ticket generated

--- 11_Browser/Project 1 - Jira Ticket Clone/script.js
+++ 11_Browser/Project 1 - Jira Ticket Clone/script.js
@@ -2,6 +2,8 @@ let addBtn = document.querySelector(".add-btn");
 
 let modalCont = document.querySelector(".modal-cont");
 
+let mainCont = document.querySelector('.main-cont')
+
 let addFlag = false;
 
 addBtn.addEventListener("click", function (e) {
@@ -18,3 +20,32 @@ addBtn.addEventListener("click", function (e) {
     modalCont.style.display = "none";
   }
 });
+
+
+
+// Generating a Ticket
+
+modalCont.addEventListener('keydown' , function(e){
+  let key = e.key
+
+  if(key=='Shift'){
+    createTicket() // this function will generate the ticket
+    modalCont.style.display='none'
+  }
+})
+
+
+
+function createTicket(){
+  let ticketCont = document.createElement('div')
+  ticketCont.setAttribute('class' , 'ticket-cont')
+
+  ticketCont.innerHTML = `<div class="ticket-color"></div>
+  <div class="ticket-id"></div>
+  <div class="task-area"></div>`
+
+  mainCont.appendChild(ticketCont)
+
+
+}
+
