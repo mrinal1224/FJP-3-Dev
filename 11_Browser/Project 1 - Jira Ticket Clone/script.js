5162b27f77ef23befafdd30b5d5f7f9b245c08e3
mrinal1224 <iammrinalkc@gmail.com>
2022-03-25 23:00:01 +0530

    push

--- 11_Browser/Project 1 - Jira Ticket Clone/script.js
+++ 11_Browser/Project 1 - Jira Ticket Clone/script.js
@@ -4,10 +4,10 @@ let modalCont = document.querySelector(".modal-cont");
 
 let mainCont = document.querySelector(".main-cont");
 
-let colors = ['lightpink' , 'lightblue' , 'lightgreen' , 'black']
-let modalPriorityColor = colors[colors.length-1] // black
+let colors = ["lightpink", "lightblue", "lightgreen", "black"];
+let modalPriorityColor = colors[colors.length - 1]; // black
 
-let allPriorityColors = document.querySelectorAll('.priority-color')
+let allPriorityColors = document.querySelectorAll(".priority-color");
 
 let addFlag = false;
 
@@ -26,21 +26,18 @@ addBtn.addEventListener("click", function (e) {
   }
 });
 
-
 //Changing Priority Colors
 
-allPriorityColors.forEach(function(colorElem){
-  colorElem.addEventListener('click' , function(e){
-       allPriorityColors.forEach(function(priorityColorElem){
-          priorityColorElem.classList.remove('active')
-       })
-       colorElem.classList.add('active')
-  })
-})
+allPriorityColors.forEach(function (colorElem) {
+  colorElem.addEventListener("click", function (e) {
+    allPriorityColors.forEach(function (priorityColorElem) {
+      priorityColorElem.classList.remove("active");
+    });
+    colorElem.classList.add("active");
 
-
-
-
+    modalPriorityColor = colorElem.classList[0];
+  });
+});
 
 // Generating a Ticket
 
@@ -48,17 +45,17 @@ modalCont.addEventListener("keydown", function (e) {
   let key = e.key;
 
   if (key == "Shift") {
-    createTicket(); // this function will generate the ticket
+    createTicket(modalPriorityColor); // this function will generate the ticket
     modalCont.style.display = "none";
-    addFlag = false
+    addFlag = false;
   }
 });
 
-function createTicket() {
+function createTicket(ticketKaColorClass) {
   let ticketCont = document.createElement("div");
   ticketCont.setAttribute("class", "ticket-cont");
 
-  ticketCont.innerHTML = `<div class="ticket-color"></div>
+  ticketCont.innerHTML = `<div class="ticket-color ${ticketKaColorClass} "></div>
   <div class="ticket-id"></div>
   <div class="task-area"></div>`;
 
