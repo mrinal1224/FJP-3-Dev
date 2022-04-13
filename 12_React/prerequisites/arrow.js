3ad8ee6a4b127a6452f5f810830c2628a300db33
mrinal1224 <iammrinalkc@gmail.com>
2022-04-13 22:26:50 +0530

    constructor functions

--- 12_React/prerequisites/arrow.js
+++ 12_React/prerequisites/arrow.js
@@ -1,7 +1,4 @@
-
-
 // // write a function to add two numbers and print their sum (function Expression)
-
 
 // let add =(a , b)=>{
 //       console.log(a+b)
@@ -9,42 +6,33 @@
 
 // add(3 , 4)
 
-
-
-
 // var regularFunction = function(name){
 //        console.log(`I am ${name}`)
 // }
 
-
 // let arrowFunction = name => console.log(`I am ${name}`)
-
-
 
 // regularFunction('Adam')
 
 // arrowFunction('Steve')
 
-
 let test = () => {
-       console.log(this) // {}
-}
- test()
+  console.log(this); // {}
+};
+test();
 
+let person = {
+  name: "Adam",
+  age: 27,
 
-// let person = {
-//        name : 'Adam',
-//        age : 27 ,
+  showDetails: function () {
+    console.log(this.name + " " + this.age);
+  },
 
-//        showDetails : function(){
-//               console.log(this.name + ' ' + this.age)
-//        },
+  arrow: () => {
+    console.log(this.name + " " + this.age);
+  },
+};
 
-//        showDetailsArrow : ()=>{
-//               console.log(this.name + ' ' + this.age)
-//        }
-// }
-
-
-// person.showDetails()
-// person.showDetailsArrow()
+person.showDetails();
+person.arrow()
