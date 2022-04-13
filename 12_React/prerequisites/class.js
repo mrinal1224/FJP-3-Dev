ff17628cb51270121dd4fc7eb5439be81bbd314d
mrinal1224 <iammrinalkc@gmail.com>
2022-04-13 23:28:09 +0530

    code

--- 12_React/prerequisites/class.js
+++ 12_React/prerequisites/class.js
@@ -2,11 +2,15 @@ class Person {
   constructor(name, age) {
     (this.name = name), (this.age = age);
   }
+
+  welcome(){
+     console.log("hello "+ this.name)
+  }
 }
 
 class Teacher extends Person {
   constructor(name , age ,classStrength) {
-    super(name, age);
+     super(name , age)
     this.classStrength = classStrength;
   }
 }
@@ -16,12 +20,20 @@ class Student extends Person {
     super(name, age);
     this.cgpa = cgpa;
   }
+
+  hello(){
+         super.welcome()
+  }
 }
 
 let person1 = new Person("Adam", 25);
 let teacher1 = new Teacher("Steve", 40, 100);
 let student1 = new Student("Mark", 21, 8.0);
 
-console.log(person1);
-console.log(teacher1);
-console.log(student1);
+// console.log(person1);
+// console.log(teacher1);
+// console.log(student1);
+
+
+person1.welcome()
+student1.hello()
