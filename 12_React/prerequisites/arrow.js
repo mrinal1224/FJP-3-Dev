eea2e65461775c8d9e1affda1725cc31842a087a
mrinal1224 <iammrinalkc@gmail.com>
2022-04-13 21:24:34 +0530

    arrow function

---
+++ 12_React/prerequisites/arrow.js
@@ -0,0 +1,23 @@
+// write a function to add two numbers and print their sum (function Expression)
+
+
+let add =(a , b)=>{
+      console.log(a+b)
+}
+
+add(3 , 4)
+
+
+let regularFunction = function(name){
+       console.log(`I am ${name}`)
+}
+
+
+let arrowFunction = (name) =>{
+       console.log(`I am ${name}`)
+}
+
+
+regularFunction('Adam')
+
+arrowFunction('Steve')
--- 12_React/prerequisites/spread/object.js
+++ 12_React/prerequisites/spread/object.js
@@ -9,13 +9,18 @@ let obj = {
   },
 };
 
-let obj2 = {...obj , address : {...obj.address , state : {...obj.address.state}}  } // shallow copy
+// let obj2 = {...obj , address : {...obj.address , state : {...obj.address.state}}  } // shallow copy
 
-obj.address.country = "India";
+// obj.address.country = "India";
 
-obj.address.state.pincode = 222222
+// obj.address.state.pincode = 222222
 
-//let obj2 = JSON.parse(JSON.stringify(obj)); // Deep Copy
+// //let obj2 = JSON.parse(JSON.stringify(obj)); // Deep Copy
 
-console.log(obj);
-console.log(obj2);
+// console.log(obj);
+// console.log(obj2);
+
+
+let test = JSON.stringify(obj)
+
+console.log(test)
