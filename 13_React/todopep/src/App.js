27f9e8280851f9431b735e8885e7b65af9e71839
mrinal1224 <iammrinalkc@gmail.com>
2022-04-18 22:11:18 +0530

    react npx

---
+++ 13_React/todopep/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Todo from "./components/Todo";
+
+function App() {
+  return <Todo />;
+}
+
+export default App;
---
+++ 13_React/todopep/src/components/Todo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class Todo extends Component {
+  render() {
+    return (
+      <div>This is Todo Component</div>
+    )
+  }
+}
+
+
+export default Todo
