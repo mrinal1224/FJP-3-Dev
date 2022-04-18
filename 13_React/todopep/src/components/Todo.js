8ececd0bcda4d15af83412e12b78c8bbcd8a9c32
mrinal1224 <iammrinalkc@gmail.com>
2022-04-18 22:40:12 +0530

    todo list

--- 13_React/todopep/src/components/Todo.js
+++ 13_React/todopep/src/components/Todo.js
@@ -1,12 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Todo extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      tasks: [
+        { task: "Get Milk", id: 1 },
+        { task: "Attend a Meeting", id: 2 },
+      ],
+      currTask: "",
+    };
+  }
+
   render() {
     return (
-      <div>This is Todo Component</div>
-    )
+      <div>
+        <input type="text" />
+        <button>Add Task</button>
+
+        <ul>
+          {this.state.tasks.map((taskObj) => (
+             <li>
+              <p>{taskObj.task}</p>
+              <button>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
-
-export default Todo
+export default Todo;
