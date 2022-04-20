c9f7ddd1e0effc3f389a219bdaee8f859ca3165e
mrinal1224 <iammrinalkc@gmail.com>
2022-04-20 23:05:30 +0530

    banner

--- 13_React/movieapp/src/components/Banner.js
+++ 13_React/movieapp/src/components/Banner.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 export class Banner extends Component {
   render() {
     return (
-       <div className="card" >
-       <img src="https://d2kektcjb0ajja.cloudfront.net/images/posts/feature_images/000/000/072/large-1466557422-feature.jpg?1466557422" className="card-img-top" alt="..."/>
-       <div className="card-body">
-         <h5 className="card-title">Card title</h5>
-         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       <div className="card banner-card" >
+       <img src="https://d2kektcjb0ajja.cloudfront.net/images/posts/feature_images/000/000/072/large-1466557422-feature.jpg?1466557422" className="card-img-top banner-img" alt="..."/>
+      
+         <h5 className="card-title banner-title">Card title</h5>
+         <p className="card-text  banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
          <a href="#" className="btn btn-primary">Go somewhere</a>
-       </div>
+      
      </div>
     )
   }
