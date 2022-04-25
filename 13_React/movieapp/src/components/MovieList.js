693ae448dd35b47a5689359be43d97653f55ffae
mrinal1224 <iammrinalkc@gmail.com>
2022-04-25 21:49:02 +0530

    movies styling

--- 13_React/movieapp/src/components/MovieList.js
+++ 13_React/movieapp/src/components/MovieList.js
@@ -6,16 +6,28 @@ import {movies} from '../movieData'
 export class MovieList extends Component {
   render() {
       let moviesArr = movies.results
-      console.log(moviesArr)
+   
 
     return (
-      <div>
-        {
-          moviesArr.map((movieElem)=>(
-            <h1>{movieElem.title}</h1>
-          ))
-        }
-      </div>
+       <>
+        <div>
+          <h3 className='text-center'> <strong>Trending</strong></h3>
+        </div>   
+        
+        <div className='movies-list'>
+                {
+                  moviesArr.map((movieElem)=>(
+                    <div className="card movie-card" >
+                    <img src="https://d2kektcjb0ajja.cloudfront.net/images/posts/feature_images/000/000/072/large-1466557422-feature.jpg?1466557422" style={{height:'40vh' , width:'20vw'}} className="card-img-top movie-img" alt="..."/>
+                   
+                      <h5 className="card-title banner-title">{movieElem.title}</h5>
+                
+                   
+                  </div>
+                  ))
+                }
+        </div>
+        </> 
     )
   }
 }
