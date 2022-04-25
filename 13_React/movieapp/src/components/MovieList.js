55c14b346757251f432b457604ced9970c0c7e53
mrinal1224 <iammrinalkc@gmail.com>
2022-04-25 21:25:02 +0530

    acess titles of mvoes

--- 13_React/movieapp/src/components/MovieList.js
+++ 13_React/movieapp/src/components/MovieList.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 
+
+import {movies} from '../movieData'
+ 
 export class MovieList extends Component {
   render() {
+      let moviesArr = movies.results
+      console.log(moviesArr)
+
     return (
-      <div></div>
+      <div>
+        {
+          moviesArr.map((movieElem)=>(
+            <h1>{movieElem.title}</h1>
+          ))
+        }
+      </div>
     )
   }
 }
