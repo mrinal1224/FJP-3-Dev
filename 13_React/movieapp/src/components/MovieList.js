c36b477e6c988814d8b48faca3a8eb81837048fb
mrinal1224 <iammrinalkc@gmail.com>
2022-04-27 19:48:44 +0530

    push

--- 13_React/movieapp/src/components/MovieList.js
+++ 13_React/movieapp/src/components/MovieList.js
@@ -1,35 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
+import { movies } from "../movieData";
 
-import {movies} from '../movieData'
- 
 export class MovieList extends Component {
   render() {
-      let moviesArr = movies.results
-   
+    let moviesArr = movies.results;
 
     return (
-       <>
+      <>
         <div>
-          <h3 className='text-center'> <strong>Trending</strong></h3>
-        </div>   
-        
-        <div className='movies-list'>
-                {
-                  moviesArr.map((movieElem)=>(
-                    <div className="card movie-card" >
-                    <img src="https://d2kektcjb0ajja.cloudfront.net/images/posts/feature_images/000/000/072/large-1466557422-feature.jpg?1466557422" style={{height:'40vh' , width:'20vw'}} className="card-img-top movie-img" alt="..."/>
-                   
-                      <h5 className="card-title banner-title">{movieElem.title}</h5>
-                
-                   
-                  </div>
-                  ))
-                }
+          <h3 className="text-center">
+            {" "}
+            <strong>Trending</strong>
+          </h3>
         </div>
-        </> 
-    )
+
+        <div className="movies-list">
+          {moviesArr.map((movieElem) => (
+            <div className="card movie-card">
+              <img
+                src="https://d2kektcjb0ajja.cloudfront.net/images/posts/feature_images/000/000/072/large-1466557422-feature.jpg?1466557422"
+                style={{ height: "40vh", width: "20vw" }}
+                className="card-img-top movie-img"
+                alt="..."
+              />
+
+              <h5 className="card-title banner-title">{movieElem.title}</h5>
+            </div>
+          ))}
+        </div>
+      </>
+    );
   }
 }
 
-export default MovieList
+export default MovieList;
