e3e66c31f082e53539dc37354bce5e6dbf55d5e6
mrinal1224 <iammrinalkc@gmail.com>
2022-05-02 22:59:40 +0530

    table complete

--- 13_React/movieapp/src/components/Favourites.js
+++ 13_React/movieapp/src/components/Favourites.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 
-import {movies} from '../movieData'
+import { movies } from "../movieData";
 
 export class Favourites extends Component {
   render() {
-         const moviesArr = movies.results
-         console.log(moviesArr)
-         let genreids = {28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
-       27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'};
+    const moviesArr = movies.results;
+    console.log(moviesArr);
+    let genreids = {
+      28: "Action",
+      12: "Adventure",
+      16: "Animation",
+      35: "Comedy",
+      80: "Crime",
+      99: "Documentary",
+      18: "Drama",
+      10751: "Family",
+      14: "Fantasy",
+      36: "History",
+      27: "Horror",
+      10402: "Music",
+      9648: "Mystery",
+      10749: "Romance",
+      878: "Sci-Fi",
+      10770: "TV",
+      53: "Thriller",
+      10752: "War",
+      37: "Western",
+    };
     return (
       <div className="main">
         <div className="row">
@@ -34,24 +53,28 @@ export class Favourites extends Component {
                     <th scope="col">genre</th>
                     <th scope="col">Popularity</th>
                     <th scope="col">Ratings</th>
-                    <th scope="col">Delete from List</th>
                   </tr>
                 </thead>
                 <tbody>
-
-                       {
-                              moviesArr.map((movieElem)=>(
-                                   <tr>
-                                   <th scope="row">{movieElem.title}</th>
-                                   <td>{genreids[movieElem.genre_ids[0]]}</td>
-                                   <td>{movieElem.popularity}</td>
-                                   <td>{movieElem.vote_average}</td>
-                                   <td><button type="button" class="btn btn-danger">Delete</button></td>
-                                   
-                                 </tr>
-                              ))
-                       }
-                 
+                  {moviesArr.map((movieElem) => (
+                    <tr>
+                      <td>
+                        <img
+                          style={{ width: "6rem" }}
+                          src={`https://image.tmdb.org/t/p/original${movieElem.backdrop_path}`}
+                        />
+                      </td>
+                      <th scope="row">{movieElem.title}</th>
+                      <td>{genreids[movieElem.genre_ids[0]]}</td>
+                      <td>{movieElem.popularity}</td>
+                      <td>{movieElem.vote_average}</td>
+                      <td>
+                        <button type="button" class="btn btn-danger">
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
