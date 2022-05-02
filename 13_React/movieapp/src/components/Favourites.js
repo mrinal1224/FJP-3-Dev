9570aa248259acfbd0b94417c8c2cb6b6ca87abd
mrinal1224 <iammrinalkc@gmail.com>
2022-05-02 22:20:59 +0530

    ui

--- 13_React/movieapp/src/components/Favourites.js
+++ 13_React/movieapp/src/components/Favourites.js
@@ -12,17 +12,75 @@ export class Favourites extends Component {
               <li class="list-group-item">Action</li>
               <li class="list-group-item">Action</li>
               <li class="list-group-item">Action</li>
-              
             </ul>
           </div>
-             <div className="col-9 favourites-table">
-                    <div className="row">
-                    <input type='text' className="input-group-text col"/>
-                    <input type='number' className="input-group-text col"/>
-                    </div>
-                
-             </div>
+          <div className="col-9 favourites-table">
+            <div className="row">
+              <input type="text" className="input-group-text col" />
+              <input type="number" className="input-group-text col" />
+            </div>
 
+            <div className="row">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <nav aria-label="Page navigation example">
+              <ul class="pagination">
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    Previous
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    1
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    2
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    3
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    Next
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     );
