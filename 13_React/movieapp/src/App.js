5a17162cbea26d63319bff8686e39f45af7f9c92
mrinal1224 <iammrinalkc@gmail.com>
2022-05-04 22:22:57 +0530

    router

--- 13_React/movieapp/src/App.js
+++ 13_React/movieapp/src/App.js
@@ -5,28 +5,24 @@ import Banner from "./components/Banner";
 import MovieList from "./components/MovieList";
 import Favourites from "./components/Favourites";
 
-import {BrowserRouter , Routes , Route } from 'react-router-dom'
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-     
       <NavBar />
 
-     <Routes>
-       <Route path='/' element={<><Banner /> <MovieList/> </>} />
-       <Route path ='/favourites' element={ <Favourites/>}/>
-     </Routes>
-
-
-
-
-
-      
-      
-     
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner /> <MovieList />
+            </>
+          }
+        />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
     </BrowserRouter>
   );
 }
